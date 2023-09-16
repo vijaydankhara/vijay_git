037a0b4bf403be7379cfd65ecaa6540352c07c75
vijay dankhara <vijaydankhara111@gmail.com>
2023-09-16 09:34:33 +0530

    16-09-2023

---
+++ .vscode/maths.cpp
@@ -0,0 +1,42 @@
+#include<math.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "< #########  CEIL  ######### >" << endl;
+    cout << "ceil number is ---> "  << ceil(3.5) << endl;
+    cout << "ceil number is ---> "  << ceil(3.1) << endl;
+    cout << "ceil number is ---> "  << ceil(3.9) << endl;
+    cout << "********************************************" << endl;
+
+    cout << "< #########  FLOOR  ######### >" << endl;
+    cout << "floor number is ---> "  << floor(5.3) << endl;
+    cout << "floor number is ---> "  << floor(5.5) << endl;
+    cout << "floor number is ---> "  << floor(5.9) << endl;
+    cout << "********************************************"  << endl;
+
+    cout << "< #########  ROUND  ######### >" << endl;
+    cout << "round number is ---> "  << round(8.9) << endl;
+    cout << "round number is ---> "  << round(8.5) << endl;
+    cout << "round number is ---> "  << round(8.2) << endl;
+    cout << "********************************************"  << endl;
+
+    cout << "< #########  SQRT  ######### >" << endl;
+    cout << "sqrt number is ---> "  << sqrt(16) << endl;
+    cout << "sqrt number is ---> "  << sqrt(4) << endl;
+    cout << "********************************************"  << endl;
+
+    cout << "< #########  POWER  ######### >" << endl;
+    cout << "power number is ---> "  << pow(4,2) << endl;
+    cout << "power number is ---> "  << pow(8,3) << endl;
+    cout << "********************************************"  << endl;
+
+    cout << "< #########  CEIL  ######### >" << endl;
+    cout << "abs number is ---> "  << abs(-8) << endl;
+    cout << "abs number is ---> "  << abs(-15) << endl;
+    cout << "abs number is ---> "  << abs(89) << endl;
+    cout << "********************************************"  << endl;
+
+    
+}
