24424f502356cdab1b6316f3badef04da6886d0d
vijay dankhara <vijaydankhara111@gmail.com>
2023-09-25 08:02:05 +0530

    25-09-2023

---
+++ hybrid_inheritance.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class Vehicle 
+{ 
+    public:
+    void display()
+    {
+        cout << "This is a vehicle." << endl;
+    }
+};
+
+class Car : public Vehicle
+{
+    public:
+    void dis() 
+    {
+        display();
+        cout << "This is a car." << endl;
+    }
+};
+
+class Bicycle  
+{
+    public:
+    void disp()
+    {
+        cout << "This is a bicycle." << endl;
+    }
+};
+
+class ElectricVehicle : public Car, public Bicycle
+{
+    public:
+    void show() 
+    {
+        dis();
+        disp();
+        cout << "This is an electric vehicle." << endl;
+    }
+};
+
+int main() 
+{
+    ElectricVehicle ev;
+    
+    ev.show();
+}
